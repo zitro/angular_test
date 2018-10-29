560b162375afd522f625172a2be56c44099d93d7
zitro <bryan@bryanortiz.com>
2018-10-29 00:29:06 -0400

    moved posts from scope to factory

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var app = angular.module('zNews', []);
 
 app.factory('posts', [function(){
-  // service body
+	var o = {
+    posts: []
+  };
+  return o;
 }])
 
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
-	$scope.posts = [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4}
-	];
+	// $scope.posts = [
+	//   {title: 'post 1', upvotes: 5},
+	//   {title: 'post 2', upvotes: 2},
+	//   {title: 'post 3', upvotes: 15},
+	//   {title: 'post 4', upvotes: 9},
+	//   {title: 'post 5', upvotes: 4}
+	// ];
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === '') { return; }
 
